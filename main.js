388dc88b0f04f7e26bb342a7096bd2f146f097ad
Peter W <techieshark@gmail.com>
2017-10-28 11:08:18 -0700

    add more functionality and better styling

--- main.js
+++ main.js
@@ -1,24 +1,5 @@
-// Set default location in case the IP doesn't have one
-let lat = 40.8;
-let lon = -96.67;
-// Grab IP location from freegeoip API
-const urlGeoIP = 'http://freegeoip.net/json/';
-// var urlStations = 'http://feeds.bayareabikeshare.com/stations/stations.json';
-// const urlStations = 'getBSjson.php';
-// const urlStations = 'http://127.0.0.1:8080/http://feeds.bayareabikeshare.com/stations/stations.json';
-const urlStations = 'http://127.0.0.1:3001/';
+const stationsURL = 'https://lit-beach-21586.herokuapp.com/'; // geojson mirror of stations api
 var map;
-
-fetch(urlGeoIP)
-  .then(resp => resp.json())
-  .then((data) => {
-    lat = data.latitude;
-    lon = data.longitude;
-    mapCreate([lon,lat]);
-  })
-  .catch((error) => {
-    console.log('Error fetching location data: ' + error);
-  });
 
 
 /**
@@ -56,43 +37,164 @@ function stationsLoad(stations) {
   });
 }
 
+
 function addStations() {
   map.on("load", function() {
-    map.addSource('stations-source', {
-      type: 'geojson',
-      data: 'https://lit-beach-21586.herokuapp.com/' // geojson mirror of stations api
+    // map.addSource('stations-source', {
+    //   type: 'geojson',
+    //   data: 'https://lit-beach-21586.herokuapp.com/'
+    // });
+
+
+  window.setInterval(function() {
+    map.getSource('stations-source').setData(stationsURL);
+    console.log('refetching live station data');
+  }, 30 * 1000); // every N seconds (in milliseconds)
+
+  map.addSource('stations-source', { type: 'geojson', data: stationsURL });
+  map.addLayer({
+      "id": "stations-layer",
+      // "type": "symbol",
+      "type": "circle",
+      "source": "stations-source",
+      "paint": {
+        "circle-radius": 12,
+        "circle-color": "#B42222",
+        "circle-opacity": 0, // transparent dots sized for interaction - allows popup
+      },
+      // "layout": {
+      //     // "icon-image": "rocket-15"
+      //     "icon-image": "bikeshare-marker",
+      //     "icon-size": 2
+      // }
+  });
+
+
+    // Change the cursor to a pointer when the mouse is over the layer.
+    map.on('mouseenter', 'stations-layer', function () {
+      map.getCanvas().style.cursor = 'pointer';
     });
 
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'stations-layer', function () {
+        map.getCanvas().style.cursor = '';
+    });
+
+
+    // TODO. For now, we've switched to getting layer from tileset in mapbox.
+    // Need to figure out how to update tileset every X minutes.
 
     // add slightly different layers for the different zoom levels
-    map.addLayer({
-      "id": "stations-icons",
-      "type": "circle",
-      "source": "stations-source",
-      "paint": {
-          // "fill-color": "#888888",
-          // "fill-opacity": 0.4
-          "circle-radius": 6,
-          "circle-color": "#B42222"
-      },
-      "minzoom": 13
-    });
+    // map.addLayer({
+    //   "id": "stations-icons",
+    //   // "type": "circle",
+    //   "source": "stations-source",
+    //   // "paint": {
+    //   //     "circle-radius": 6,
+    //   //     "circle-color": "#B42222"
+    //   // },
+    //   type: 'symbol',
+    //   // source: 'bikeshare-stations',
+    //   "layout": {
+    //       "icon-image": "bikeshare-marker",
+    //       "icon-size":1.5
+    //   },
+    //   "paint": {},
+    //   "minzoom": 13
+    // });
 
-    map.addLayer({
-      "id": "stations-dots",
-      "type": "circle",
-      "source": "stations-source",
-      "paint": {
-          // "fill-color": "#888888",
-          // "fill-opacity": 0.4
-          "circle-radius": 2,
-          "circle-color": "#B42222"
-      },
-      // "filter": ["==", "$type", "Polygon"]
-      "maxzoom": 13
-    });
+    // map.addLayer({
+    //   "id": "stations-dots",
+    //   "type": "circle",
+    //   "source": "stations-source",
+    //   "paint": {
+    //       "circle-radius": 2,
+    //       "circle-color": "#B42222"
+    //   },
+    //   "maxzoom": 13
+    // });
 
   });
+}
+
+function addPopups() {
+  map.on('click', function(e) {
+    var features = map.queryRenderedFeatures(e.point, {
+      layers: ['stations-layer'] // replace this with the name of the layer
+    });
+
+    if (!features.length) {
+      return;
+    }
+
+    var feature = features[0];
+
+    var popup = new mapboxgl.Popup({ offset: [0, -15] })
+      .setLngLat(feature.geometry.coordinates)
+      .setHTML(getPopupContent(feature))
+      .setLngLat(feature.geometry.coordinates)
+      .addTo(map);
+    popup._container.classList.add('station-popup--container'); // for styling
+  });
+}
+
+
+/**
+ * Get HTML content describing a station.
+ * @param station - one station from the API
+ */
+function getPopupContent(station) {
+  const {
+    stAddress1: addr,
+    latitude: lat,
+    longitude: lng,
+    availableBikes: bikes,
+    availableDocks: docks,
+    statusValue: status } = station.properties;
+  const directionsURL = getDirectionsLink(addr, undefined, lat, lng);
+  let round = (n) => Number(n).toFixed(2);
+
+  const alertMsg = (status === 'Not In Service') ? `<div class="station-popup--alert">${status}</div>` : '';
+
+  return `
+    <div class="station-popup">
+      <h3>${addr}</h3>
+      ${alertMsg}
+      <div class="columns station-popup--stats">
+        <div class="column station-popup--bikes">
+          <div class="station-popup--bikes-number">${bikes}</div>
+          <div class="station-popup--bikes-text">bikes</div>
+        </div>
+        <div class="column station-popup--docks">
+          <div class="station-popup--docks-number">${docks}</div>
+          <div class="station-popup--docks-text">docks</div>
+        </div>
+      </div>
+      <div class="station-popup--directions">
+        <a rel="noopener noreferrer" target="_blank" href="${directionsURL}">Directions to here</a>
+      </div>
+      <div class="station-popup--coordinates">Lat/Long: <abbr title="${lat}, ${lng}">${round(lat)}, ${round(lng)}</abbr></div>
+    </div>`
+}
+
+/**
+ * Get URL for Google directions to or from a given place.
+ * @param {*} dir - either 'to' (default) or 'from'
+ * @param {*} addr - address
+ * @param {*} lat - latitude
+ * @param {*} lng - longitude
+ */
+function getDirectionsLink(toAddr = '', fromAddr = '', toLat, toLng) {
+  // TODO / BUG: "addresses" with a '/' in them don't work, like
+  // "Civic Center/UN Plaza BART Station (Market St at McAllister St)" - currently that results
+  // in directions from civic center to UN plaza (b/c g-maps separates locations with a '/')
+  const baseURL = 'https://www.google.com/maps/dir';
+  const coords = toLat ? `${toLat},${toLng},`: '';
+  const zoom = 17;
+  return `${baseURL}/${fromAddr}/${toAddr}/@${coords}${zoom}/`
+  // Google maps expects addresses with name first, then plus-separated components like this:
+  // Noisebridge,+2169+Mission+St,+San+Francisco,+CA+94110
+
 }
 
 /**
@@ -102,46 +204,33 @@ function addStations() {
 function mapCreate(center) {
   map = new mapboxgl.Map({
     container: 'map',
-    // style: 'mapbox://styles/mapbox/streets-v9',
-    style: 'mapbox://styles/mapbox/light-v9',
-    // style: 'mapbox://styles/techieshark/cj96g0w8c59zk2snztf4ild9v',
+    style: 'mapbox://styles/techieshark/cj97690r00bcc2sthkqxn8f2v',
     zoom: '13',
     center: center,
   });
 
   // fetchStations();
   addStations();
+  addPopups();
 }
 
-// map.addControl(new mapboxgl.FullscreenControl());
 
+(function init() {
+  // Set default location in case the IP doesn't have one
+  let lat = 40.8;
+  let lon = -96.67;
 
-
-
-
-/// OLD CRUD
-
-
-// var request = new XMLHttpRequest();
-// request.open('GET', urlGeoIP, true);
-
-// request.onload = function() {
-//   if (request.status >= 200 && request.status < 400) {
-//     // Success!
-//     var json = JSON.parse(request.responseText);
-//     lat = json.latitude;
-//     lon = json.longitude;
-//   } else {
-//     // We reached our target server, but it returned an error
-//     console.log('error determining coordinates from IP');
-//   }
-//   mapCreate([lon,lat]);
-// };
-
-// request.onerror = function() {
-//   // There was a connection error of some sort
-//   console.log('error connecting to geoIP server');
-//   mapCreate([lon,lat]);
-// };
-
-// request.send();
+  // Grab IP location from freegeoip API
+  const geoLocationProviderURL = 'http://freegeoip.net/json/';
+  fetch(geoLocationProviderURL)
+    .then(resp => resp.json())
+    .then((data) => {
+      lat = data.latitude;
+      lon = data.longitude;
+      mapCreate([lon,lat]);
+    })
+    .catch((error) => {
+      console.log('Error fetching location data: ' + error);
+      mapCreate([lon,lat]); // go for it anyway, using defaults
+    });
+})();
