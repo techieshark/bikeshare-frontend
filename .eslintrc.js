fd00ef7ffc7d87a3d2c8bb5a384d53d4f68e53bf
Peter W <techieshark@gmail.com>
2017-10-30 16:51:57 -0700

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     "extends": "airbnb-base",
     "globals": {
         "document": false,
+        "navigator": false,
         "window": false,
         "fetch": false,
         "mapboxgl": false,
