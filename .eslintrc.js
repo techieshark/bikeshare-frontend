f1e8d3ddc6dc6e039a6f1ea02dd9e5d28852f22e
Peter W <techieshark@gmail.com>
2017-11-01 15:10:09 -0700

    init dist

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "globals": {
+        "document": false,
+        "navigator": false,
+        "window": false,
+        "fetch": false,
+        "mapboxgl": false,
+        "$": false,
+    }
+};
