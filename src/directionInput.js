7bea86e00aba775eac55c521a0ca469dec5e8218
Peter W <techieshark@gmail.com>
2017-11-01 12:52:17 -0700

    fix weird chrome bug
    (state values accessed through local name
     weren't updating)

--- src/directionInput.js
+++ src/directionInput.js
@@ -8,15 +8,13 @@ import state from './state';
  * @param {String} location - 'origin' or 'destination'; requires state[destination] to exist.
  */
 function geocodingChangeHandler(location) {
-  const stateLocation = state[location];
-
   return function changeEventHandler(event) {
     const addr = event.target.value;
     if (addr === '') {
       // user cleared input - erase known point for location
-      stateLocation.longitude = null;
-      stateLocation.latitude = null;
-      stateLocation.address = null;
+      state[location].longitude = null;
+      state[location].latitude = null;
+      state[location].address = null;
       mapUpdateDirectionsEndpoint(location);
       return;
     }
@@ -28,15 +26,12 @@ function geocodingChangeHandler(location) {
           d.type === 'FeatureCollection' && d.features && d.features.length > 0
         ) {
           if (d.features[0].place_name) {
-            stateLocation.address = d.features[0].place_name;
+            state[location].address = d.features[0].place_name;
           }
           if (d.features[0].center) {
-            [stateLocation.longitude, stateLocation.latitude] = d.features[0].center;
             console.log('gecoder returned coords:', [stateLocation.longitude, stateLocation.latitude]);
+            [state[location].longitude, state[location].latitude] = d.features[0].center;
           }
-          // XXX weird hack to ensure chrome updates state.
-          // stateLocation.newthing = 1;
-          // state[location].anothernewthing = 2;
           console.log('app state:', state);
           mapUpdateDirectionsEndpoint(location);
         }
