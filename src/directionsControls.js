b6d7a0a0e309f181e89a2a6aaba93d2bf824080f
Peter W <techieshark@gmail.com>
2017-11-02 01:01:56 -0700

    add toggler for directions

--- src/directionsControls.js
+++ src/directionsControls.js
@@ -4,9 +4,29 @@ import initDistanceSlider from './distanceSlider';
 import initOriginLocatorBtn from './originLocatorButton';
 import initDirectionInput from './directionInput';
 
+function initDirectionsToggle() {
+  const toggle = document.getElementById('directions-toggle');
+  let shown = true;
+  toggle.onclick = () => {
+    const content = document.getElementsByClassName('directions--content')[0];
+    if (shown) {
+      // first ensure the toggle button doesn't disappear
+      toggle.style.width = content.offsetWidth + 'px';
+      content.style.display = 'none';
+    } else {
+      toggle.style.width = '100%';
+      content.style.display = 'block';
+    }
+    shown = !shown;
+    toggle.classList.toggle('shown');
+    toggle.classList.toggle('hidden');
+  };
+}
+
 export default function initDirectionsControls() {
   initDistanceSlider();
   initOriginLocatorBtn();
   initDirectionInput('originInput', 'origin');
   initDirectionInput('destinationInput', 'destination');
+  initDirectionsToggle();
 }
