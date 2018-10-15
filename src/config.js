b6f3e6980e2e70adc240ef0f99d5436119d39444
Peter W <techieshark@gmail.com>
2018-10-15 00:01:33 -0700

    update to bikeshare specific Mapbox key

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  mapboxToken: 'pk.eyJ1IjoidGVjaGllc2hhcmsiLCJhIjoiYzk2ZEFWTSJ9.8ZY6rG2BWXkDBmvAPvn_nw',
+  mapboxToken: 'pk.eyJ1IjoidGVjaGllc2hhcmsiLCJhIjoiY2o5NmVpOTI1MDNxMjJ2cGJuZG44eTExOSJ9.K2lI3DUzNvAD3LAAtfwlBA',
   mapStyle: 'mapbox://styles/techieshark/cj97690r00bcc2sthkqxn8f2v',
   stationsUrl: 'https://lit-beach-21586.herokuapp.com/', // geojson mirror of stations api
 };
